ad0193a8389a893100b07e1add482788353190e4
softmattertheory <timothy.atherton@tufts.edu>
2025-05-16 13:05:19 -0400

    Move to new version number

--- src/build.h
+++ src/build.h
@@ -10,11 +10,11 @@
  * Version
  * ********************************************************************** */
 
-#define MORPHO_VERSIONSTRING "0.6.2-newdiscretization"
+#define MORPHO_VERSIONSTRING "0.6.3"
 
 #define MORPHO_VERSION_MAJOR 0
 #define MORPHO_VERSION_MINOR 6
-#define MORPHO_VERSION_PATCH 2
+#define MORPHO_VERSION_PATCH 3
 
 /* **********************************************************************
  * Paths and file system
